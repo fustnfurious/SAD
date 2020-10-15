9a29a19c519430bc79f74561c88619f815aaa62a
Lluís Fuster <lluis.lfb@gmail.com>
2020-10-15 17:34:20 +0200

    Create columnCounter.java

---
+++ columnCounter.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class columnCounter {
+	
+	
+	public static void main(String[] args) {
+		getColumns();
+	}
+	public static int getColumns() {
+		String cmd = "tput cols";
+		int cols=0;
+		try {
+			Process pr = Runtime.getRuntime().exec(cmd);
+			BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			cols = Integer.parseInt(in.readLine());
+			System.out.println("Hi ha "+ cols + " columnes.");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return cols;
+	}
+}

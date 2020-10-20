4ac9bd9c4f6edb08da71f5a854e1db211f403a9d
Lluís Fuster <lluis.lfb@gmail.com>
2020-10-20 18:14:32 +0200

    Add files via upload

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -2,19 +2,55 @@
 import java.io.*;
 import java.util.ArrayList;
 public class EditableBufferedReader extends BufferedReader{
-	public final int LEFT = 279168;
-	public final int RIGHT = 279167;
+	public final int LEFT = 68; //^[[D
+	public final int RIGHT = 67;//^[[C
 	public final int BKSPC = 127;
-	public final int SUP = 279151126;
+	public final int SUP = 51; //^[[3~
 	public final int ENTER = 13;
-	public final int INS = 279150126;
+	public final int INS = 50; //^[[2~
+	public final int HOME = 72; //^[[H
+	public final int END = 70; //^[[F
 	public final int CtrlD = 4;
-	int[] sca = {LEFT, RIGHT, BKSPC, SUP, INS, CtrlD};
+	public final int ESC = 27;
+	
+	public final int _LEFT = -1;
+	public final int _RIGHT = -2;
+	public final int _BKSPC = -3;
+	public final int _SUP = -4;
+	public final int _ENTER = -5;
+	public final int _INS = -6; 
+	public final int _HOME = -7;
+	public final int _END = -8;
+	public final int _CtrlD = -9;
+	public final int _ERR=-10;
+	
+	
+	int[] sca = {_LEFT, _RIGHT, _BKSPC, _SUP, _INS, _CtrlD, _HOME, _END, _ERR};
 	protected Line line;
-	protected int pos=0;
 	
 	public EditableBufferedReader(InputStreamReader in) {
 		super(in);
+	}
+	
+	public int tradueix() {
+		int c = read();
+		switch (c) {
+		case CtrlD: return _CtrlD;
+		case BKSPC: return _BKSPC;
+		case ESC: 
+			c=read();
+			c=read();
+			switch(c) {
+			case LEFT: return _LEFT;
+			case RIGHT: return _RIGHT;
+			case SUP: return _SUP;
+			case INS: return _INS;
+			case HOME: return _HOME;
+			case END: return _END;
+			default: return _ERR;
+			}
+		default: return c;
+		}
 	}
 	
 	
@@ -23,39 +59,38 @@ public class EditableBufferedReader extends BufferedReader{
 		setRaw();
 		line = new Line();
 		int c=0;
-		while (c!=CtrlD) {
+		while (c!=_CtrlD) {
 			try {
-				c=read();
+				c=tradueix();
 				if (isSpecialCharacter(c)) {
 					switch (c) {
-					case LEFT:
-						if(pos!=0) {
-							pos--;
-						}
+					case _LEFT:
+						line.arrowRight();
 						break;
-					case RIGHT:
-						if(pos<line.getSize()) {
-							pos++;
-						}
+					case _RIGHT:
+						line.arrowRight();
 						break;
-					case BKSPC:
-						line.backSpace(pos);
-						pos--;
+					case _BKSPC:
+						line.backSpace();
 						break;
-					case INS:
+					case _INS:
 						break;
-					case SUP:
-						line.supr(pos);
-						pos--;
+					case _SUP:
+						line.supr();
 						break;
-					case CtrlD:
+					case _HOME:
+						line.home();
+						break;
+					case _END:
+						line.end();
+						break;
+					case _ERR:
 						break;
 					
 					}
 				} else {
 					char b = (char) c;
-					line.add(pos, b);
-					pos++;
+					line.add(b);
 					System.out.print(c);
 				}
 			} catch (IOException e) {
@@ -76,17 +111,12 @@ public class EditableBufferedReader extends BufferedReader{
 		}
 	}
 	public void unsetRaw() {
-		String[] cmd = {"/bin/sh", "-c", "stty cooked </dev/tty"};
+		String[] cmd = {"/bin/sh", "-c", "stty echo cooked </dev/tty"};
 		try {
 			Runtime.getRuntime().exec(cmd);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	
-	public void actualitzaVista() {
-		
 	}
 	
 	public Boolean isSpecialCharacter(int c) {
--- Line.java
+++ Line.java
@@ -2,25 +2,51 @@ import java.util.ArrayList;
 
 public class Line {
 	protected ArrayList<Character> line;
+	int pos;
+	boolean insMode;
 	
 	public Line() {
 		line = new ArrayList<Character>();
+		int pos = 0;
+		insMode = false;
 	}
 	
-	public void backSpace(int pos) {
+	public void arrowLeft() {
 		if (pos!=0) {
-			line.remove(pos-1);
+			pos--;
 		}
 	}
 	
-	public void supr(int pos) {
-		if (pos!=0) {
-			line.remove(pos-1);
+	public void arrowRight() {
+		if (pos<this.line.size()-1) {
+			pos++;
+		}
+	}
+	public void backSpace() {
+		if (this.pos!=0) {
+			line.remove(this.pos-1);
+			pos--;
+		}
+		
+	}
+	
+	public void home() {
+		pos=0;
+	}
+	
+	public void end() {
+		pos= line.size()-1;
+	}
+	
+	public void supr() {
+		if(this.pos<line.size()-1) {
+		line.remove(this.pos);
 		}
 	}
 	
-	public void add(int pos, char c) {
-		line.add(pos, c);
+	public void add(char c) {
+		line.add(this.pos, c);
+		pos++;
 	}
 	
 	public int getSize() {

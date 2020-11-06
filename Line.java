1d992d3197088676ddd9454408f1bc8a4b1e93c1
Lluís Fuster <lluis.lfb@gmail.com>
2020-11-06 23:09:15 +0100

    Add class Line versio Obsevable

--- Line.java
+++ Line.java
@@ -1,31 +1,34 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Observable;
 
-public class Line {
+@SuppressWarnings("deprecation")
+public class Line extends Observable {
+	
 	protected ArrayList<Character> line;
-	protected View view;
-	int pos;
+	int pos=0;
 	boolean overrideMode;
 	
 	public Line() {
 		line = new ArrayList<Character>();
-		view = new View();
-		int pos = 0;
 		overrideMode = false;
 	}
 	
 	public void arrowLeft() {
 		if (pos!=0) {
-			view.left();
+			//view.left();
 			pos--;
+			setChanged();
+			notifyObservers(new Changes(true, Changes.LEFT, 1));
 		}
 		
 	}
 	
 	public void arrowRight() {
 		if (pos<this.line.size()) {
-			view.right();
+			//view.right();
 			pos++;
+			setChanged();
+			notifyObservers(new Changes(true, Changes.RIGHT, 1));
 		}
 	}
 	public void backSpace() {
@@ -33,31 +36,38 @@ public class Line {
 			if(this.pos!=line.size()) { //si no esborra des del final
 				line.remove(this.pos-1);
 				pos--;
-				view.left();
+				/*view.left();
 				for(int i=pos; i<line.size();i++) { // imprimim la resta de la linia
 					view.write(line.get(i));
 				}
 				view.write((char)32); //espai
-				view.home(line.size()-pos +1);
+				view.home(line.size()-pos +1);*/
 			} else { 					// si esborra des del final
 				line.remove(this.pos-1);
 				pos--;
-				view.left();
+				/*view.left();
 				view.write((char)32); //espai
-				view.left();
+				view.left();*/
 			}
 		}
 		
 	}
 	
 	public void home() {
-		view.home(pos);
+		//view.home(pos);
+		int posInc = pos;
 		pos=0;
+		setChanged();
+		notifyObservers(new Changes(true, Changes.LEFT, posInc));
+		
 	}
 	
 	public void end() {
-		view.end(line.size()-pos);
+		//view.end(line.size()-pos);
+		int posInc = line.size()-pos;
 		pos=line.size();
+		setChanged();
+		notifyObservers(new Changes(true, Changes.RIGHT, posInc));
 	}
 	
 	public void switchMode() {
@@ -66,40 +76,37 @@ public class Line {
 	public void sup() {
 		if (this.pos!=line.size()) {
 				line.remove(this.pos);
-				for(int i=pos; i<line.size();i++) { // imprimim la resta de la linia
+				/*for(int i=pos; i<line.size();i++) { // imprimim la resta de la linia
 					view.write(line.get(i));
 				}
 				view.write((char)32); //espai
-				view.home(line.size()-pos+1);
+				view.home(line.size()-pos+1);*/
 		}
 	}
 	
 	public void add(char c) {
 		if(overrideMode) {
-			if (pos==line.size()) {
+			if (pos==line.size()) {// final de la linia
 				line.add(c);
 				pos++;
-				view.write(c);
+				//view.write(c);
 			} else {
 				line.set(pos, c);
 				pos++;
-				view.write(c);
+				//view.write(c);
 			}
 		} else {
-			if (pos==line.size()) {
-				line.add(c);
-				pos++;
-				view.write(c);
-			} else {
 				line.add(pos,c);
-				for(int i=pos; i<line.size();i++) { // imprimim la resta de la linia
+				pos++;
+				setChanged();
+				notifyObservers(new Changes(false, Changes.RIGHT, 1, Changes.ADD, c));
+				//view.write(c);
+				
+				/*for(int i=pos; i<line.size();i++) { // imprimim la resta de la linia
 					view.write(line.get(i));
 				}
 				view.write((char)32); //espai
-				view.home(line.size()-pos);
-				pos++;
-				
-			}
+				view.home(line.size()-pos);*/
 		}
 	}
 	
